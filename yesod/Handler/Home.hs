dfd616a11d8c41e07505d447ab6a2cba929a6ed6
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-04-11 10:27:21 +0900

    clean function yconnect

--- yesod/Handler/Home.hs
+++ yesod/Handler/Home.hs
@@ -9,7 +9,7 @@ import qualified Data.Text.IO as Txt
 
 import Crypto.Random
 
-import qualified Data.ByteString as BS
+-- import qualified Data.ByteString as BS
 import qualified Data.ByteString.Base64.URL as B64
 
 directory :: FilePath
@@ -20,41 +20,35 @@ getNonce = (Txt.dropWhileEnd (== '=') . decodeUtf8 . B64.encode <$>)
 	. getRandomBytes
 
 -- Define our data that will be used for creating the form.
-data FileForm = FileForm
-    { fileInfo :: FileInfo
-    , fileDescription :: Text
-    }
+data FileForm = FileForm {
+	fileInfo :: FileInfo,
+	fileDescription :: Text }
 
-yconnect :: MonadHandler m => Text -> Text -> Text -> Text -> m a
-yconnect stt nnc cid ruri = redirect $
-	"https://auth.login.yahoo.co.jp/yconnect/v1/authorization?" <>
-	"response_type=code+id_token&" <>
-	"scope=openid+profile&" <>
-	"client_id=" <> cid <> "&" <>
-	"state=" <> stt <> "&" <>
-	"nonce=" <> nnc <> "&" <>
-	"redirect_uri=" <> ruri
-
--- This is a handler function for the GET request method on the HomeR
--- resource pattern. All of your resource patterns are defined in
--- config/routes
---
--- The majority of the code you will write in Yesod lives in these handler
--- functions. You can spread them across multiple files if you are so
--- inclined, or create a single monolithic file.
-getHomeR :: Handler Html
-getHomeR = do
+yconnect :: Text -> Text -> Handler Html
+yconnect cid ruri = do
 	(state, nonce, date) <- lift
 		$ (,,) <$> getNonce 256 <*> getNonce 256 <*> getCurrentTime
 	_ <- runDB $ insert $ OpenIdStateNonce state nonce date
 	runDB (selectList ([] :: [Filter OpenIdStateNonce]) [])
 		>>= mapM_ print
---	runDB $ deleteWhere ([] :: [Filter OpenIdStateNonce])
-	clientId <- lift $ Txt.concat . Txt.lines
-		<$> Txt.readFile (directory </> "clientId.txt")
-	redirectUri <- lift $ Txt.concat . Txt.lines
-		<$> Txt.readFile (directory </> "redirectUri.txt")
-	yconnect state nonce clientId redirectUri
+	yc state nonce
+	where
+	yc :: MonadHandler m => Text -> Text -> m a
+	yc stt nnc = redirect $
+		"https://auth.login.yahoo.co.jp/yconnect/v1/authorization?" <>
+		"response_type=code+id_token&" <>
+		"scope=openid+profile&" <>
+		"client_id=" <> cid <> "&" <>
+		"state=" <> stt <> "&" <>
+		"nonce=" <> nnc <> "&" <>
+		"redirect_uri=" <> ruri
+
+getHomeR :: Handler Html
+getHomeR = (uncurry yconnect =<<) . lift $ (,)
+	<$> (Txt.concat . Txt.lines
+		<$> Txt.readFile (directory </> "clientId.txt"))
+	<*> (Txt.concat . Txt.lines
+		<$> Txt.readFile (directory </> "redirectUri.txt"))
 
 postHomeR :: Handler Html
 postHomeR = do
