78cac18c64b781f45fff5ecc011c24daab7bd086
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-04-13 22:43:25 +0900

    test cookie

--- yesod/Handler/Home.hs
+++ yesod/Handler/Home.hs
@@ -2,14 +2,25 @@ module Handler.Home where
 
 import Import (
 	Handler, Html,
-	($), (=<<), (<$>), (<*>),
-	uncurry, lift, getClientId, getRedirectUri,
+	($), (>>=), (=<<), (<$>), (<*>),
+	Maybe(..), uncurry, lift, print,
+	getClientId, getRedirectUri,
 	widgetFile, defaultLayout, setTitle,
+	lookupSession, setSession,
+	lookupCookie, setCookie, def,
 	Route(..))
-import OpenIdCon (yconnect)
+
+import Web.Cookie (SetCookie(..))
 
 getHomeR :: Handler Html
 getHomeR = do
+	lookupSession "hoge" >>= print
+	setSession "hoge" "hige"
+	lookupCookie "hoge" >>= print
+	setCookie def {
+		setCookieName = "hoge",
+		setCookieValue = "hidebu",
+		setCookieMaxAge = Just 10 }
 	defaultLayout $ do
 		setTitle "Welcome To Skami3!"
 		$(widgetFile "homepage")
