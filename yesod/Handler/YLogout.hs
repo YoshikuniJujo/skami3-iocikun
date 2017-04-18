e7cb0c8c491573a415bbfa4205951588d465be12
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-04-18 17:22:57 +0900

    refactoring Handler/YLogout.hs

--- yesod/Handler/YLogout.hs
+++ yesod/Handler/YLogout.hs
@@ -1,22 +1,17 @@
 module Handler.YLogout (getYLogoutR) where
 
-import Import hiding ((==.), delete)
-
-import Database.Esqueleto
+import Import (
+	Handler, Html, Text,
+	($), (.), (>>=), maybe, return, runDB, redirect, lookupCookie )
+import Model (EntityField(..))
+import Database.Esqueleto ((^.), (==.), delete, from, where_, val)
 
 getYLogoutR :: Handler Html
 getYLogoutR = do
-	session <- lookupCookie "session"
-	autoLogin <- lookupCookie "auto-login"
-	flip (maybe $ return ()) session $ \ssn -> runDB $ do
-		delete . from $ \s -> do
-			where_ $ s ^. SessionSession ==. val ssn
-	flip (maybe $ return ()) autoLogin $ \al -> runDB $ do
-		delete . from $ \a -> do
-			where_ $ a ^. AutoLoginAutoLogin ==. val al
+	(lookupCookie "session" >>=) . maybe (return ()) $ \s ->
+		runDB . delete . from
+			$ where_ . (==. val s) . (^. SessionSession)
+	(lookupCookie "auto-login" >>=) . maybe (return ()) $ \al ->
+		runDB . delete . from
+			$ where_ . (==. val al) . (^. AutoLoginAutoLogin)
 	redirect ("/" :: Text)
-			{-
-	defaultLayout $ do
-		setTitle "Welcome To Skami3!"
-		$(widgetFile "ylogout")
-		-}
