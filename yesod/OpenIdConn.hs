0ba1a1beb0e40bbb23e7eda633e113bc368e28fa
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-04-22 08:44:13 +0900

    refactoring OpenIdConn.hs (en route)

--- yesod/OpenIdConn.hs
+++ yesod/OpenIdConn.hs
@@ -20,11 +20,11 @@ import Database.Esqueleto (
 	Value(..), (^.), (==.), select, insert, delete, from, where_, val )
 
 import Import.NoFoundation (
-	Eq, Show, MonadIO, MonadThrow, Monad, liftIO, first,
+	Eq, Show, MonadIO, MonadThrow, Monad, liftIO, first, IsString, Semigroup,
 	Maybe(..), Either(..), Text, ByteString, String, HashMap,
 	Profile(..), Value(..), RequestBody(..), HeaderName,
 	($), (.), (<$>), (<$), (<*>), (<*), (=<<), (>>),
-	(/=), (==), (<), (<>), (++), (-),
+	(/=), (==), (<), (<>), (-),
 	flip, maybe, either, uncurry, fst, foldr, return, lift, when,
 	putStrLn, toRational, fromRational, mod,
 	encodeUtf8, decodeUtf8, method, runDB, redirect, lookupGetParam )
@@ -48,29 +48,30 @@ import qualified Data.ByteString.Base64.URL as B64
 import qualified Data.HashMap.Lazy as HML
 import qualified Data.Aeson as Aeson
 
+authUri, userinfoUri :: IsString s => s
+authUri = "https://auth.login.yahoo.co.jp/yconnect/v1/"
+userinfoUri = "https://userinfo.yahooapis.jp/yconnect/v1/"
+
 yconnect :: ClientId -> RedirectUri -> MyHandler a
 yconnect cid ruri = do
 	(state, nonce, date) <- lift
 		$ (,,) <$> getRand 256 <*> getRand 256 <*> getCurrentTime
 	_ <- runDB . insert
 		$ OpenIdStateNonce (rndToTxt state) (rndToTxt nonce) date
-	redirect $
-		"https://auth.login.yahoo.co.jp/yconnect/v1/authorization?" <>
-		"response_type=code+id_token&" <>
-		"scope=openid+profile+email&" <>
-		"client_id=" <> cidToTxt cid <> "&" <>
-		"state=" <> rndToTxt state <> "&" <>
-		"nonce=" <> rndToTxt nonce <> "&" <>
-		"redirect_uri=" <> ruToTxt ruri <> "&" <>
-		"bail=1"
+	redirect $ authUri <> "authorization?" <> Txt.intercalate "&" [
+		betw "=" "response_type" "code+id_token",
+		betw "=" "scope" "openid+profile+email",
+		betw "=" "client_id" $ cidToTxt cid,
+		betw "=" "state" $ rndToTxt state,
+		betw "=" "nonce" $ rndToTxt nonce,
+		betw "=" "redirect_uri" $ ruToTxt ruri,
+		betw "=" "bail" "1" ]
 
 authenticate :: ClientId -> ClientSecret -> RedirectUri ->
 	MyHandler (Either String (UserId, AccessToken))
 authenticate cid csc ruri = runExceptT $ do
-	c <- maybe (throwE "NO CODE") (return . Code)
-		=<< lift (lookupGetParam "code")
-	s <- maybe (throwE "NO STATE") (return . State)
-		=<< lift (lookupGetParam "state")
+	c <- (Code <$>) $ myb "NO CODE" =<< lift (lookupGetParam "code")
+	s <- (State <$>) $ myb "NO STATE" =<< lift (lookupGetParam "state")
 	n0 <- getNonceFromState s
 	(ua, tbc) <- requestUserId cid csc ruri c
 	now <- liftIO getPOSIXTime
@@ -92,47 +93,43 @@ getNonceFromState (State s) = do
 requestUserId :: (MonadIO m, MonadThrow m) =>
 	ClientId -> ClientSecret -> RedirectUri -> Code ->
 	ExceptT String m ((UserId, AccessToken), ToBeChecked)
-requestUserId cid cs ruri code = do
-	mrsp :: Maybe Aeson.Object <- lift
+requestUserId cid csc ruri code = do
+	rsp :: Aeson.Object <- myb "BAD RESPONSE" =<< lift
 		. (Aeson.decode . getResponseBody <$>)
-		. httpLBS
-		. setMethodHeadersBody
-			"POST"
-			[contentType, basicAuthentication cid cs]
-			(	"grant_type=authorization_code&" <>
-				"code=" <> codeToBs code <> "&" <>
-				"redirect_uri=" <> ruToBs ruri )
-		=<< parseRequest authUri
-	rsp <- maybe (throwE "BAD RESPONSE") return mrsp
-	at <- maybe (throwE "NO ACCESS_TOKEN") (return . AccessToken)
+		. httpLBS . setMethodHeadersBody "POST"
+			[contentType, basicAuthentication cid csc]
+			(BS.intercalate "&" [
+				betw "=" "grant_type" "authorization_code",
+				betw "=" "code" $ codeToBs code,
+				betw "=" "redirect_uri" $ ruToBs ruri ])
+		=<< parseRequest (authUri <> "token")
+	at <- (AccessToken <$>) . maybe (throwE "NO ACCESS_TOKEN") return
 		$ unstring =<< HML.lookup "access_token" rsp
-	tt <- maybe (throwE "NO TOKEN_TYPE") (return . TokenType)
+	tt <- (TokenType <$>) . maybe (throwE "NO TOKEN_TYPE") return
 		$ unstring =<< HML.lookup "token_type" rsp
 	it <- maybe (throwE "NO ID_TOKEN") return
 		$ unstring =<< HML.lookup "id_token" rsp
 	first (, at) <$> getUidAndTbc tt it
-	-- ^
-	-- |
-	-- Use ZipList
 
-authUri :: String
-authUri = "https://auth.login.yahoo.co.jp/yconnect/v1/token"
+betw :: Semigroup m => m -> m -> m -> m
+betw = ((<>) .) . flip (<>)
+
+myb :: Monad m => e -> Maybe a -> ExceptT e m a
+myb = flip maybe return . throwE
 
 contentType :: (HeaderName, [ByteString])
 contentType = ("Content-Type", ["application/x-www-form-urlencoded"])
 
 basicAuthentication :: ClientId -> ClientSecret -> (HeaderName, [ByteString])
-basicAuthentication cid' cs' = (
+basicAuthentication cid csc = (
 	"Authorization",
-	["Basic " <> B64.encode (cidToBs cid' <> ":" <> csToBs cs')] )
+	["Basic " <> B64.encode (cidToBs cid <> ":" <> csToBs csc)] )
 
 setMethodHeadersBody ::
 	ByteString -> [(HeaderName, [ByteString])] -> ByteString ->
 	Request -> Request
-setMethodHeadersBody m h b r0 =
-	let	r1 = foldr (uncurry setRequestHeader) r0 { method = m } h
-		r2 = (`setRequestBody` r1) $ RequestBodyBS b in
-		r2
+setMethodHeadersBody m hs b r0 = setRequestBody (RequestBodyBS b)
+	$ foldr (uncurry setRequestHeader) r0 { method = m } hs
 
 getUidAndTbc :: Monad m =>
 	TokenType -> Text -> ExceptT String m (UserId, ToBeChecked)
@@ -216,7 +213,7 @@ payloadToAeson (Payload t) = either throwE return $ toAeson t
 
 toAeson :: Text -> Either String Aeson.Object
 toAeson t = do
-	b <- left ("B64.decode error: " ++)
+	b <- left ("B64.decode error: " <>)
 		. B64.decode $ encodeUtf8 padded
 	maybe (Left "Aeson.decode error") Right
 		. Aeson.decode $ LBS.fromStrict b
@@ -246,9 +243,7 @@ codeToBs (Code c) = encodeUtf8 c
 getProfile ::
        (MonadIO m, MonadThrow m) => AccessToken -> m (Maybe Aeson.Object)
 getProfile (AccessToken at) = do
-       initReq <- parseRequest $
-               "https://userinfo.yahooapis.jp/yconnect/v1/attribute?" <>
-               "schema=openid"
+       initReq <- parseRequest $ userinfoUri <> "attribute?schema=openid"
        let     req = setRequestHeader
                        "Authorization" ["Bearer " <> encodeUtf8 at] initReq
        rBody <- getResponseBody <$> httpLBS req
